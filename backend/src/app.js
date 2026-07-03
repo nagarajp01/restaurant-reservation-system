51ed53cb27a34ee02fb88705587a0426a04c8198
nagarajp01 <nagarajpatlolla1@gmail.com>
2026-07-03 16:23:45 +0530

    Setup Express server and middleware

---
+++ backend/src/app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({
+    extended: true,
+    limit: "16kb"
+}));
+app.use(cookieParser());
+app.use(morgan("dev"))
+
+app.get("/", (req, res) => {
+    res.status(200).json({
+        message: "Restaurant Reservation API is running"
+    });
+});
+
+export { app };
