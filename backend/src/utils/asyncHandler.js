2ee4123b95de938e21c49106384a0ec0f874cb1c
nagarajp01 <nagarajpatlolla1@gmail.com>
2026-07-03 17:40:50 +0530

    Added utility classes for API responses and error handling

---
+++ backend/src/utils/asyncHandler.js
@@ -0,0 +1,15 @@
+const asyncHandler = (requestHandler) => {
+    return async (req, res, next) => {
+        try {
+            await requestHandler(req, res, next);
+        } catch (error) {
+            return res.status(error.statusCode || 500).json({
+                success: false,
+                message: error.message || "Internal Server Error",
+                errors: error.errors || [],
+            });
+        }
+    };
+};
+
+export { asyncHandler };
